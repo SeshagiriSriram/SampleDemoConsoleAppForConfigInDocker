24b82c5865c98a8dbb93fa93054f4f353295c61f
Seshagiri Sriram <Sriram@cogniphi.com>
2021-10-08 16:24:29 +0530

    first commit

---
+++ DemoConsoleAppForConfigInDocker/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Configuration; 
+using System.Collections.Generic;
+
+namespace DemoConsoleAppForConfigInDocker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var appSettings = SystemConfigurationLoader.Instance.GetAppSettings(); 
+            Console.WriteLine("Config value: "+ appSettings["Demo"]??"Unset"); 
+        }
+    }
+}
---
+++ DemoConsoleAppForConfigInDocker/SystemConfigurationLoader.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Configuration;
+using System.IO;
+
+namespace DemoConsoleAppForConfigInDocker
+{
+    public  class SystemConfigurationLoader
+    {
+        public SystemConfigurationLoader()
+        {
+            System.Configuration.ExeConfigurationFileMap fileMap = new ExeConfigurationFileMap();
+            fileMap.ExeConfigFilename = "./App.config";
+            System.Configuration.Configuration configuration = System.Configuration.ConfigurationManager.OpenMappedExeConfiguration(fileMap, ConfigurationUserLevel.None);
+            foreach (var key in configuration.AppSettings.Settings.AllKeys)
+            {
+                try
+                {
+                    System.Configuration.ConfigurationManager.AppSettings[key] = configuration.AppSettings.Settings[key].Value;
+                }
+                catch (Exception)
+                {
+                   
+                }
+            }
+            Console.WriteLine("Loaded System Configuration");
+        }
+        public static SystemConfigurationLoader Instance { get { return NestedInstance.instance; } }
+
+        private class NestedInstance
+        {
+            static NestedInstance()
+            {
+
+            }
+            internal static readonly SystemConfigurationLoader instance = new SystemConfigurationLoader(); 
+        }
+        public System.Collections.Specialized.NameValueCollection GetAppSettings()
+        {
+            return System.Configuration.ConfigurationManager.AppSettings; 
+        }
+    }
+}
+
